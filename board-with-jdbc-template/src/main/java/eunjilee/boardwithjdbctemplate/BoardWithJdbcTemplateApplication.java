9a468e3e699e02017713759cf776c120e6380737
USER2 <user2@AD01937884.local>
2023-05-02 23:26:18 +0900

    initiating spring

---
+++ board-with-jdbc-template/src/main/java/eunjilee/boardwithjdbctemplate/BoardWithJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package eunjilee.boardwithjdbctemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardWithJdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardWithJdbcTemplateApplication.class, args);
+	}
+
+}
---
+++ board-with-jdbc-template/src/test/java/eunjilee/boardwithjdbctemplate/BoardWithJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package eunjilee.boardwithjdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardWithJdbcTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
